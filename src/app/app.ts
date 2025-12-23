481656cdc47f869c2315e4f49e845b0069bd69a4
shamsutty <shamsutty2419@gmail.com>
2025-12-23 10:52:54 +0530

    testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{ title() }}!</h1>
-
+    This is shamsutty
     <router-outlet />
   `,
   styles: [],
